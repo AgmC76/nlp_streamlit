f4f4521692c7f6e5aae425286d4206ee3f029532
AgmC76 <167646477+AgmC76@users.noreply.github.com>
2026-04-30 00:35:42 -0400

    added test samples

--- Assignment6.py
+++ Assignment6.py
@@ -77,4 +77,44 @@ y = df["species"]
 model = RandomForestClassifier()
 model.fit(X, y)
 
+
+# Generate test samples from user inputs. 
+# Predict the flower species based for the given input (sepal length, sepal width, petal length and petal width)
+# Generate test samples from user inputs
+if "df" not in st.session_state:
+    st.session_state.df = pd.DataFrame({'sepal length (cm)': [], 
+                                        'sepal width (cm)' : [], 
+                                        'petal length (cm)': [], 
+                                        'petal length (cm)': [], 
+                                        'petal width (cm)' : []})
+
+st.write("Enter values for input features:")
+with st.form("add_row_form"):
+    sepal_length = st.slider("Sepal length", float(df['sepal length (cm)'].min()), float(df['sepal length (cm)'].max()))
+    sepal_width  = st.slider("Sepal width",  float(df['sepal width (cm)'].min()),  float(df['sepal width (cm)'].max()))
+    petal_length = st.slider("Petal length", float(df['petal length (cm)'].min()), float(df['petal length (cm)'].max()))
+    petal_width  = st.slider("Petal width",  float(df['petal width (cm)'].min()),  float(df['petal width (cm)'].max()))
+
+    submitted = st.form_submit_button("Predict")
+
+    if submitted:
+        # Prediction            
+        input_data = [[sepal_length, sepal_width, petal_length, petal_width]]
+        prediction = model.predict(input_data)
+        predicted_species = target_name[prediction[0]]
+
+        new_row = pd.DataFrame([{'sepal length (cm)': sepal_length, 
+                                 'sepal width (cm)' : sepal_width, 
+                                 'petal length (cm)': petal_length, 
+                                 'petal width (cm)': petal_width, 
+                                 'predicted species' : predicted_species}])
+        
+        st.session_state.df = pd.concat([st.session_state.df, new_row], ignore_index=True)
+        
+        # Display the prediction
+        st.success(f"Row added!   Predicted species is {prediction} {predicted_species}")
+
+st.write("Predicted species for tested samples:")
+st.dataframe(st.session_state.df)
+
 # streamlit run nlp_app.py
