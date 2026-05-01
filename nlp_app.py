36d2fca1978a7d9c7bda7a30631f03fa52f87004
AgmC76 <167646477+AgmC76@users.noreply.github.com>
2026-05-01 15:46:53 -0400

    Comment out test string in nlp_app.py
    Commented out test string for clarity.

--- nlp_app.py
+++ nlp_app.py
@@ -10,10 +10,9 @@ st.title("NLP Streamlit")
 text = st.text_area("Enter something")
 
 #test string:
-"""
-My name is jeff, i am going on a walk. Yesterday I walked five miles. 
-I have been wanting to get in shape, so I have been making a point to exercise regularly. 
-"""
+# My name is jeff, i am going on a walk. Yesterday I walked five miles. 
+# I have been wanting to get in shape, so I have been making a point to exercise regularly. 
+
 # downloading the nltk wordset
 nltk.download('wordnet')
 
